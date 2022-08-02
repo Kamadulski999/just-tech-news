da6661d9b94d7e1eba06c91d1b5e864d2379b196
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-08-02 13:36:41 -0500

    MVP

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
+    order: [['created_at', 'DESC']],
     attributes: [
       'id',
       'post_url',
@@ -12,8 +13,15 @@ router.get('/', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']], 
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -40,17 +48,19 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
-  {
-    model: Post,
-    attributes: ['id', 'title', 'post_url', 'created_at']
-  },
-  {
-    model: Post,
-    attributes: ['title'],
-    through: Vote,
-    as: 'voted_posts'
-  }
-]
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
   })
     .then(dbPostData => {
       if (!dbPostData) {
@@ -64,6 +74,7 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
 
 // create new Post
 router.post('/', (req, res) => {
